928bcad2d7aa4538a933c6526430abc9c887341c
mutabazifidele <mutabazifidele@yahoo.com>
2022-01-04 13:01:47 +0100

    assignment4 starter code

--- examples/Lecture40/js/script.js
+++ examples/Lecture40/js/script.js
@@ -1,1 +1,22 @@
-var x = "Hello World!";
+var string = "Hello";
+string +=" World";
+console.log(string + "!");
+
+console.log((5+4)/3);
+console.log(undefined/5);
+
+function test1(a){
+  console.log(a/5);
+}
+
+test1("k")
+// if(x == undefined){
+//   console.log("x is undefined");
+// }
+//
+// x = 5;
+// if (x == undefined) {
+//   console.log("x is undefined");
+// }else {
+//   console.log("x has been defined!, x= " + x);
+// }
